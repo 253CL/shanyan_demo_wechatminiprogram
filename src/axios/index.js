a20792dfed94866e600e4146e7e802717710cc21
CLYZ_CS\Thinkpad <815335804@qq.com>
2024-03-25 14:31:03 +0800

    update

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ const exampleobj = axios.create({
     withCredentials: true,
     // baseURL: 'https://api.253.com/open/i/htjc/'
     // baseURL: 'http://172.16.40.52:18080'  
-    baseURL: 'flash_test/sy/h5/init'  
+    baseURL: 'flash_h5_api/sy/h5/init'  
 });
 
 // http request 拦截器
