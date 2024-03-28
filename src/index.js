1dbc0226b17f4ad964b6a780486249909496e2cc
CLYZ_CS\Thinkpad <815335804@qq.com>
2024-03-28 17:50:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -261,7 +261,7 @@ function Main({ params, callback }) {
     }, [cmcc]);
     return (
         <React.Fragment>
-            <button style={{ display: 'none' }} id="j-get-code">
+            <button style={{ display: 'none' }} id="j-get-code" onClick={()=>{console.log(123123123);}}>
                 电信校验本机号码
             </button>
             {cuccView && (
